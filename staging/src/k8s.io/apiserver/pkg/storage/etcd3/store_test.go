kubernetes/apiserver

Add context to Count()
Passing the same context will let us associate etcd traces with those
from apiserver.

Signed-off-by: Aleksander Mistewicz <amistewicz@google.com>

Kubernetes-commit: 96b39187c5fac62e462dc348ccc1e3938464d9e1

pkg/storage/etcd3/store_test.go
@@ -692,7 +692,7 @@ func TestInvalidKeys(t *testing.T) {
 	expectInvalidKey("Get", store.Get(ctx, invalidKey, storage.GetOptions{}, nil))
 	expectInvalidKey("GetList", store.GetList(ctx, invalidKey, storage.ListOptions{}, nil))
 	expectInvalidKey("GuaranteedUpdate", store.GuaranteedUpdate(ctx, invalidKey, nil, true, nil, nil, nil))
-	_, countErr := store.Count(invalidKey)
+	_, countErr := store.Count(t.Context(), invalidKey)
 	expectInvalidKey("Count", countErr)
 }
 

pkg/storage/testing/store_tests.go
@@ -3130,7 +3130,7 @@ func RunTestCount(ctx context.Context, t *testing.T, store storage.Interface) {
 		}
 	}
 
-	resourceACountGot, err := store.Count(resourceA)
+	resourceACountGot, err := store.Count(t.Context(), resourceA)
 	if err != nil {
 		t.Fatalf("store.Count failed: %v", err)
 	}

AwesomePatrol/kubernetes

staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
@@ -692,7 +692,7 @@ func TestInvalidKeys(t *testing.T) {
 	expectInvalidKey("Get", store.Get(ctx, invalidKey, storage.GetOptions{}, nil))
 	expectInvalidKey("GetList", store.GetList(ctx, invalidKey, storage.ListOptions{}, nil))
 	expectInvalidKey("GuaranteedUpdate", store.GuaranteedUpdate(ctx, invalidKey, nil, true, nil, nil, nil))
-	_, countErr := store.Count(invalidKey)
+	_, countErr := store.Count(context.Background(), invalidKey)
 	expectInvalidKey("Count", countErr)
 }
 

staging/src/k8s.io/apiserver/pkg/storage/testing/store_tests.go
@@ -3130,7 +3130,7 @@ func RunTestCount(ctx context.Context, t *testing.T, store storage.Interface) {
 		}
 	}
 
-	resourceACountGot, err := store.Count(resourceA)
+	resourceACountGot, err := store.Count(context.Background(), resourceA)
 	if err != nil {
 		t.Fatalf("store.Count failed: %v", err)
 	}